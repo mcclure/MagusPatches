f37429284518981af09683dc4fc0e998275ae51b
mcc <andi.m.mcclure@gmail.com>
2020-11-01 12:05:24 -0500

    ScreenSaver patch

---
+++ README.md
@@ -0,0 +1,5 @@
+Patches for the [Rebel Technology OWL](https://www.rebeltech.org/) by Andi McClure <<andi.m.mcclure@gmail.com>>. Patches were tested with the [Magus](https://www.rebeltech.org/product/magus/) and some of them use the screen. There is also a PC-side test harness.
+
+Build instructions in [run.txt](run.txt). Or you can paste the .hpp files into [the RebelTech patches website](https://www.rebeltech.org/patch-library).
+
+Licenses are included in their individual files but are mostly CC0/public domain. If you redistribute these programs as source code, it would be *polite* to retain the attribution even though (for the CC0 files) you are not legally required to do so.
---
+++ ScreenSaverPatch.hpp
@@ -0,0 +1,91 @@
+#ifndef __ScreenSaver_hpp__
+#define __ScreenSaver_hpp__
+
+// Does nothing. Prints numbers to the screen. To test drawing.
+// Author Andi McClure. License https://creativecommons.org/publicdomain/zero/1.0/
+
+#include "OpenWareMidiControl.h"
+#include "math.h"
+//#include "VoltsPerOctave.h"
+
+#define ACCUMULATOR_START 0x9A9A9A9A
+#define ACCUMULATOR_MULT  0xA9A9A9A9
+#define ACCUMULATOR_ADD   0x87654321
+#define ACCUMULATOR_SHIFT 8
+
+#define BUFFER_SIZE 33
+
+class ScreenSaverPatch : public Patch {
+private:
+  uint32_t frame;
+  uint32_t accumulator; // RNG
+  char tempBuffer[BUFFER_SIZE];
+public:
+  ScreenSaverPatch(){        
+    frame = 1;
+    accumulator = ACCUMULATOR_START;
+    nextRandom(); nextRandom();
+  }
+
+  ~ScreenSaverPatch(){
+  }
+
+  void processMidi(MidiMessage msg){
+  }
+
+  void buttonChanged(PatchButtonId bid, uint16_t value, uint16_t samples){
+  }
+
+  uint32_t nextRandom() {
+    accumulator *= ACCUMULATOR_MULT;
+    accumulator += ACCUMULATOR_ADD;
+    accumulator >>= ACCUMULATOR_SHIFT;
+    return accumulator;
+  }
+
+  char *a2c(uint32_t n) {
+    char *at = &tempBuffer[BUFFER_SIZE-1];
+    *at = '\0';
+    do  {
+      *(--at) = '0' + (n % 10);
+      n /= 10;
+    } while (n);
+    return at;
+  }
+
+  void processAudio(AudioBuffer& buffer){
+    FloatArray left = buffer.getSamples(LEFT_CHANNEL);
+    FloatArray right = buffer.getSamples(RIGHT_CHANNEL);
+
+    // Buffers
+    int size = min(left.getSize(), right.getSize());
+    float *leftData = left.getData();
+    float *rightData = right.getData();
+
+    // Write sample
+    memset(leftData,  0, size*sizeof(float));
+    memset(rightData, 0, size*sizeof(float));
+  }
+
+#ifdef OWL_MAGUS
+  void processScreen(ScreenBuffer& screen){
+    int height = screen.getHeight();
+    int width = screen.getWidth();
+    uint32_t entropy = nextRandom();
+
+    bool invert = entropy & 1;
+    entropy >>= 1;
+    uint32_t y = entropy % height;
+    entropy /= height;
+    uint32_t x = entropy % width;
+
+    const char *num = a2c(frame++);
+    screen.print(x, y, num);
+  }
+#else
+  #error "This patch doesn't really work without a screen"
+#endif
+
+};
+
+#endif   // __Saw4Patch_hpp__
