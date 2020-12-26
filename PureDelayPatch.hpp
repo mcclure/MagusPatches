69ec3e5d9cb23776d26c83b5ba47ae1acc22bfca
mcc <andi.m.mcclure@gmail.com>
2020-12-26 15:59:08 -0500

    Pure Delay

--- PureDelayPatch.hpp
+++ PureDelayPatch.hpp
@@ -1,15 +1,23 @@
 #ifndef __PureDelayPatch_hpp__
 #define __PureDelayPatch_hpp__
 
-// 4 saws, each with its own detune and phase-offset controls.
-// Author Andi McClure. License https://creativecommons.org/publicdomain/zero/1.0/
+// MIDI-controllable delay, no feedback. Monitor in left channel.
+// Author Andi McClure. License https://opensource.org/licenses/MIT
 
 #include "OpenWareMidiControl.h"
 #include "math.h"
 #include "support/midi.h"
 //#include "VoltsPerOctave.h"
 
-#define BUFSIZE 44100*4
+#define BUFSIZE (44100*2+1)
+
+#define BASEDELAY     (patchForSlot(PARAMETER_A))
+#define MICRODELAY    (patchForSlot(PARAMETER_A+4))
+#define MIDIDELAY     (patchForSlot(PARAMETER_A+8))
+#define INPUTLOUD     (patchForSlot(PARAMETER_A+12))
+#define OUTPUTLOUD    (patchForSlot(PARAMETER_A+13))
+#define MONITORLOUD   (patchForSlot(PARAMETER_A+14))
+#define COMONITORLOUD (patchForSlot(PARAMETER_A+15))
 
 class PureDelayPatch : public Patch {
 private:
@@ -28,10 +36,25 @@ public:
     midinote = MIDDLEC_MIDI;
     writePtr = 0;
     memset(history, 0, sizeof(history));
+    registerParameter(BASEDELAY, "Base Delay");
+    setParameterValue(BASEDELAY, 0.5);
+    registerParameter(MICRODELAY, "Micro Delay");
+    setParameterValue(MICRODELAY, 0.5);
+    registerParameter(MIDIDELAY, "MIDI amplitude");
+    setParameterValue(MIDIDELAY, 0.5);
+    registerParameter(INPUTLOUD, "Input attenuation");
+    setParameterValue(INPUTLOUD, 1.0);
+    registerParameter(OUTPUTLOUD, "Output attenuation");
+    setParameterValue(OUTPUTLOUD, 1.0);
+    registerParameter(MONITORLOUD, "Monitor attenuation");
+    setParameterValue(MONITORLOUD, 1.0);
+    registerParameter(COMONITORLOUD, "Monitor output mix");
+    setParameterValue(COMONITORLOUD, 1.0);
   }
 
   ~PureDelayPatch(){
   }
+
 
 #ifndef OWL_SIMULATOR
   void processMidi(MidiMessage msg){
@@ -48,9 +71,18 @@ public:
   void buttonChanged(PatchButtonId bid, uint16_t value, uint16_t samples){
   }
 
+#define PCLAMP(x) max(0.0f, (min(1.0f, (x))))
+
   int backLook() {
-    return 44100;
+    float base  = getParameterValue(BASEDELAY);
+    float micro = getParameterValue(MICRODELAY);
+    float midi  = getParameterValue(MIDIDELAY);
+    float across = PCLAMP(base + (micro-0.5f)/16.0 + midi*(midinote-MIDDLEC_MIDI)/64.0f);
+
+    return across*(BUFSIZE-1);
   }
+
+#define CLAMP(x) max(-1.0f, (min(1.0f, (x))))
 
   void processAudio(AudioBuffer& buffer){
     FloatArray left = buffer.getSamples(LEFT_CHANNEL);
@@ -61,26 +93,31 @@ public:
     float *leftData = left.getData();
     float *rightData = right.getData();
 
+    float inputLoud = getParameterValue(INPUTLOUD);
+    float outputLoud = getParameterValue(OUTPUTLOUD);
+    float monitorLoud = getParameterValue(MONITORLOUD);
+    float comonitorLoud = getParameterValue(COMONITORLOUD);
+
     for(int i = 0; i < size; i++ ) {
-      float sample = max(-1.0f, (min(1.0f, leftData[i] + rightData[i])));
+      float sample = CLAMP((leftData[i] + rightData[i])*inputLoud);
 
       history[writePtr++] = sample;
       writePtr %= BUFSIZE;
     }
 
     int back = backLook();
-    int from = writePtr - back - size + BUFSIZE;
+    int from = writePtr - back - size + 2*BUFSIZE;
     for(int i = 0; i < size; i++ ) {
       int at = (from+i)%BUFSIZE;
-      float sample = history[at];
+      float sample = history[at]*outputLoud;
       
-      leftData[i] = sample;
+      leftData[i] = CLAMP(leftData[i]*monitorLoud + sample*comonitorLoud);
       rightData[i] = sample;
     }
   }
 
 #ifdef USE_SCREEN
-  void processScreen(ScreenBuffer& screen){ // Print notes-down stack
+  void processScreen(ScreenBuffer& screen){ // TODO
   }
 #endif
 
